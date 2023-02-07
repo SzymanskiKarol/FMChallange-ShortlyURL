833faaab3ba744093b725fbf0e7aca6b1417c8f9
SzymanskiKarol <szymanski.karol.1991@gmail.com>
2023-02-07 16:49:12 +0100

    input validation added

--- app.js
+++ app.js
@@ -9,24 +9,55 @@ document.addEventListener("DOMContentLoaded", getLinks);
 shortenBtn.addEventListener("click", addNewLink);
 
 function addNewLink() {
-    fetch(`https://api.shrtco.de/v2/shorten?url=${linkToShortEl.value}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('Success:', data);
-            console.log(data.result.short_link);
-            let newDiv = document.createElement("div");
-            newDiv.innerHTML = `<a class="long-link" href="${linkToShortEl.value}" target="_blank">${linkToShortEl.value}</a>
+    if (linkToShortEl.value) {
+        fetch(`https://api.shrtco.de/v2/shorten?url=${linkToShortEl.value}`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+                console.log(data.result.short_link);
+                let newDiv = document.createElement("div");
+                newDiv.innerHTML = `<a class="long-link" href="${linkToShortEl.value}" target="_blank">${linkToShortEl.value}</a>
                 <div class="short-link-container-right">
                 <a href="https://${data.result.short_link}" class="short-link" target="_blank">${data.result.short_link}</a>
-                <button class="copy">Copy</button></div>`
-            newDiv.classList.add("short-link-container")
-            shortenLinksSection.appendChild(newDiv);
-            saveLocalLink(newDiv.innerHTML)
-            linkToShortEl.value = "";
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+                <button class="copy">Copy</button>
+                <button class="remove">x</button></div>`
+                newDiv.classList.add("short-link-container")
+
+                let linksArr;
+                if (localStorage.getItem('linksArr') === null) {
+                    linksArr = [];
+                } else {
+                    linksArr = JSON.parse(localStorage.getItem('linksArr'))
+                }
+
+                if (!linksArr.includes(newDiv.innerHTML)) {
+                    console.log(newDiv.innerHTML);
+                    saveLocalLink(newDiv.innerHTML)
+                    shortenLinksSection.appendChild(newDiv);
+                    linkToShortEl.value = "";
+                    console.log("okej");
+                } else {
+                    console.log("not okej");
+                    linkToShortEl.classList.add("error");
+                    document.querySelector(".message").innerText = "This link is already shortened"
+                    setTimeout(() => {
+                        linkToShortEl.classList.remove("error");
+                        document.querySelector(".message").innerText = ""
+                    }, 2000)
+                }
+
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    } else {
+        linkToShortEl.classList.add("error");
+        document.querySelector(".message").innerText = "Please add link"
+        setTimeout(() => {
+            linkToShortEl.classList.remove("error");
+            document.querySelector(".message").innerText = ""
+        }, 2000)
+    }
 }
 
 function saveLocalLink(link) {
@@ -70,6 +101,16 @@ function getLinks() {
                 copyBtn.classList.remove("clicked");
                 copyBtn.innerText = "Copy";
             }, 1000)
+        })
+    })
+
+    let removeButtons = document.querySelectorAll(".remove");
+    removeButtons.forEach(function (removeBtn) {
+        removeBtn.addEventListener("click", function () {
+            let elToDel = removeBtn.parentElement.parentElement.innerHTML;
+            linksArr.splice(linksArr.indexOf(elToDel), 1);
+            localStorage.setItem("linksArr", JSON.stringify(linksArr))
+            removeBtn.parentElement.parentElement.remove();
         })
     })
 }
